da88d4a456534a3ffce771cda78646a0b7127910
carazzim0 <basti.carazzimo@gmail.com>
2016-09-22 09:39:32 +0200

    switch to supervisord in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-##########
-# Config #
-##########
+# /**********************/
+# /*                    */
+# /*       CONFIG       */
+# /*                    */
+# /**********************/
+SUPERVISORD_CONFIG='/etc/supervisor/conf.d/supervisord.conf'
+
 MYSQL_HOST="${MYSQL_HOST:-}"
 MYSQL_PORT="${MYSQL_PORT:-}"
 MYSQL_USER="${MYSQL_USER:-}"
@@ -15,14 +19,16 @@ if [ -f $TS_INJECTS/licensekey.dat ]; then
 fi
 
 # modify db settings
-sed -i "s/host=.*/host=$MYSQL_HOST/g" $TS_HOME/ts3db_mariadb.ini
-sed -i "s/port=.*/port=$MYSQL_PORT/g" $TS_HOME/ts3db_mariadb.ini
-sed -i "s/username=.*/username=$MYSQL_USER/g" $TS_HOME/ts3db_mariadb.ini
-sed -i "s/password=.*/password=$MYSQL_PASS/g" $TS_HOME/ts3db_mariadb.ini
-sed -i "s/database=.*/database=$MYSQL_DB/g" $TS_HOME/ts3db_mariadb.ini
+sed -i "s|host=.*|host=$MYSQL_HOST|g" $TS_HOME/ts3db_mariadb.ini
+sed -i "s|port=.*|port=$MYSQL_PORT|g" $TS_HOME/ts3db_mariadb.ini
+sed -i "s|username=.*|username=$MYSQL_USER|g" $TS_HOME/ts3db_mariadb.ini
+sed -i "s|password=.*|password=$MYSQL_PASS|g" $TS_HOME/ts3db_mariadb.ini
+sed -i "s|database=.*|database=$MYSQL_DB|g" $TS_HOME/ts3db_mariadb.ini
+# adjust supervisord config
+sed -i "s|{%TS_HOME%}|$TS_HOME|g; s|{%TS_USER%}|$TS_USER|g" $SUPERVISORD_CONFIG
 
-chown -R teamspeak. $TS_HOME $TS_INJECTS
+chown -R $TS_USER. $TS_HOME $TS_INJECTS
 
-cd $TS_HOME
-
-$TS_HOME/ts3server_minimal_runscript.sh inifile=$TS_HOME/ts3server.ini
+# wait for the database to come up
+while ! nc -z $MYSQL_HOST $MYSQL_PORT; do sleep 3; done
+/usr/bin/supervisord -c $SUPERVISORD_CONFIG
